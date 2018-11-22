6dd35a56751e733f81d6a7e4aaebe29573e10ad5
carterwilson <carterlwilson@gmail.com>
2018-11-22 08:45:33 -0800

    Added basic Mdirectory functionality, started on file reading.

--- chocan/MDirectory.java
+++ chocan/MDirectory.java
@@ -1,6 +1,51 @@
 package chocan;
+import java.util.*;
+import java.io*;
 
 public class MDirectory
 {
+    public Map<Integer, Data> MDir = new HashMap();
 
+    public int verifyMember(int mid){
+        if (MDir.containsKey){
+            return 1;
+        }
+        else return 0;
+    }
+
+    public int addMember(Member nMember){
+        MDir.put(nMember.number, nMember);
+        return 0;
+    }
+
+    public int removeMember(Member int mid){
+        MDir.remove(mid);
+        return 0;
+    }
+
+    public void buildReports(){
+        for (Member value: MDir.values()){
+            value.buildReport();
+        }
+    }
+
+    public void readIn(){
+        String lastName;
+        String firstName;
+        int phone;
+        String address;
+        String city;
+        String state;
+        int zip;
+
+        Scanner members = new Scanner("ChocAn/data/MemberList.txt").useDelimiter("#");
+
+        lastName = members.next();
+        firstName = members.next();
+        phone = members.nextInt();
+        address = members.next();
+        city = members.next();
+        state = members.next();
+        zip = members.nextInt();
+    }
 }
