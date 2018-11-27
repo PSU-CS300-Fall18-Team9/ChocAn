82738d104eca186cdab221cbe8515b0554c2a9aa
cosimogonnelli <gonnellic@gmail.com>
2018-11-26 16:56:27 -0800

    work on methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,32 +19,42 @@ public class Main {
 
         protected service next; //TODO next here???
 
-        protected int currentData;
+        protected int currentDate;
         protected int currentTime;
-        protected int serviceData;
+        protected int serviceDate;
         protected int pid;
         protected int mid;
         protected int serviceCode;
         protected int fee;
         protected String comments;
 
+        //TODO check on this, used to support main
+        protected int month;
+        protected int day;
+        protected int year;
+
         //default constructor
         public service(){
-            this.currentData = 0;
+            this.currentDate = 0;
             this.currentTime = 0;
-            this.serviceData = 0;
+            this.serviceDate = 0;
             this.pid = 0;
             this.mid = 0;
             this.serviceCode = 0;
             this.fee = 0;
             this.comments = null;
+
+            //TODO check on this, used to support main
+            this.month = 0;
+            this.day = 0;
+            this.year = 0;
         }
 
         //constructor with args
-        public service(int currentData, int currentTime, int serviceData, int pid, int mid, int serviceCode, int fee, String comments){
-            this.currentData = currentData;
+        public service(int currentDate, int currentTime, int serviceDate, int pid, int mid, int serviceCode, int fee, String comments){
+            this.currentDate = currentDate;
             this.currentTime = currentTime;
-            this.serviceData = serviceData;
+            this.serviceDate = serviceDate;
             this.pid = pid;
             this.mid = mid;
             this.serviceCode = serviceCode;
@@ -52,11 +62,20 @@ public class Main {
             this.comments = comments;
         }
 
+        //TODO constructor with limited args to support the menu functionality TODO check on this
+        public service(int month, int day, int year, int serviceCode, String comments){
+            this.month = month;
+            this.day = day;
+            this.year = year;
+            this.serviceCode = serviceCode;
+            this.comments = comments;
+        }
+
         //used as copy constructor
         public service(service toAdd){
-            this.currentData = toAdd.currentData;
+            this.currentDate = toAdd.currentDate;
             this.currentTime = toAdd.currentTime;
-            this.serviceData = toAdd.serviceData;
+            this.serviceDate = toAdd.serviceDate;
             this.pid = toAdd.pid;
             this.mid = toAdd.mid;
             this.serviceCode = toAdd.serviceCode;
@@ -64,27 +83,53 @@ public class Main {
             this.comments = toAdd.comments;
         }
 
-        //change date
+        //display is implemented using toString with System.out.println(srv);
+        //anytime we want to display a service fields
+        public String toString()
+        {
+            String info;
+
+            info = "\nCurrent Date: " + this.currentDate +
+                    "\nCurrent Time: " + this.currentTime +
+                    "\nService Date: " + this.serviceDate +
+                    "\nService Code: " + this.serviceCode +
+                    "\nPid: " + this.pid +
+                    "\nMid: " + this.mid +
+                    "\nFee: " + this.fee +
+                    "\nComment: " + this.comments;
+
+            return info;
+        }
+
+        //compare svcCode to Service code
+        protected boolean find(int svcCode)
+        {
+            if(serviceCode == svcCode) return true;
+
+            return false;
+        }
+
+        //wrapper for constructor to et called in main
+        public boolean createServiceRec(int month, int day, int year, int svcCode, String comment)
+        {
+            new service(month, day, year, svcCode, comment);
+            return true;
+        }
+
+        //change date TODO do we need this???
         public int changeDate(int serviceDate){
 
+            return 1;
         }
 
-        //wrapper for display TODO we need a display function??
-        public int displayName(){
-            if(this.head == null) return 0;
+        //add comments TODO doe we need this ???
+        public int addComments(String comment){
 
-            return displayName(this.head.goNext())+1;
+            return 1;
         }
 
-        //display name TODO what do we need to do with this...
-        protected int displayName(node head){
-                System.out.println(("Name: " + Name));
-        }
-
-        //add comments
-        public String addComments(String comment){
-        }
-
+        /*
+        //TODO I don't think we need this also get everything as string and constructor expects int
         //make provider report
         protected int makeProReport(PDirectory PDir){
             try {
@@ -114,7 +159,7 @@ public class Main {
                     comments = tokenizer.nextToken();
 
                     //TODO call a function to create an obj of type service
-                    service temp = new service(currentData, currentTime, serviceData, pid, mid, serviceCode, fee, comments)
+                 //   service temp = new service(currentData, currentTime, serviceData, pid, mid, serviceCode, fee, comments) //TODO takes int String passed
                     //TODO or retrive an obj???
 
                 }
@@ -125,9 +170,11 @@ public class Main {
             }
             return -1;
         }
-
+*/
+        //TODO to implement this??? need it???
         //make member report
         protected int makeMemReport(){
+            return 1;
         }
     }
 }
