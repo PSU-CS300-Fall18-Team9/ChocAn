25588e2c7ddf2e1a0a56aa0d34118ce7a307769a
carterwilson <carterlwilson@gmail.com>
2018-11-26 21:24:49 -0800

    Added build report, read to file, functionality.

--- chocan/MDirectory.java
+++ chocan/MDirectory.java
@@ -48,4 +48,58 @@ public class MDirectory
         state = members.next();
         zip = members.nextInt();
     }
+
+    public String toString(){
+        String strArray;
+
+        for(Map.Entry<Integer, Provider> entry: MDir.entrySet()){
+            strArray = entry.getValue().report();
+            StringBuilder nString = new StringBuilder();
+            nString.append("Member Name: " + strArray[0] + " " strArray[1] + "\n");
+            nString.append("Member Number: " + strArray[2] + "\n");
+            nString.append("Address: " + strArray[3] + "\n");
+            nString.append("City: " + strArray[4] + "\n");
+            nString.append("State: " + strArray[5] + "\n");
+            nString.append("Zip: " + strArray[6] + "\n");
+
+            StringBuilder dataFile = new StringBuilder();
+
+            dataFile.append("./reports/member/" + tData[0]+tData[1] + ".txt");
+
+            File outFile = new File(dataFile.toString());
+            PrintWriter pw = new PrintWrite(outFile);
+            pw.write(nString.toString());
+            pw.close();
+        }
+
+    }
+
+    public void initialize(){
+        String dataFile = "./data/MemberList.txt";
+        String line = "";
+        String delim = "#";
+
+        try{
+            FileReader fileIn = new FileReader(dataFile);
+            BufferedReader buffIn = new BufferedReader(fileIn);
+        }
+
+        while ((line = buffIn.readLine()) != null){
+            String[] tData = line.split(delim);
+
+            Member nMember = new Member(tData[0], tData[1], tData[2], tData[3], tData[4], tData[5], tData[6], tData[7]);
+            this.addMember(nMember);
+        }
+
+        fileIn.close();
+
+        catch (IOException e1)
+        {
+            System.out.println("Exception thrown:" + e1);
+        }
+    }
+
+    public saveFile(){
+
+    }
 }
